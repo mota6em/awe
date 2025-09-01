861632c6cd929778acbe506ae73e6e486a55faa0
Motasem AbuBaraka <mota6em@gmail.com>
2025-09-01 15:34:35 +0200

    feat: add menu sheet for mobile views

--- src/components/TobBar.tsx
+++ src/components/TobBar.tsx
@@ -5,16 +5,25 @@ import Documents from "../pages/Documents";
 import Events from "../pages/Events";
 import NonMuslimsDay from "../pages/Non-muslims-day";
 import Sds from "../pages/Sds";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const TopBar = () => {
   return (
     <>
-      <header className="flex items-center justify-between px-6 py-2 bg-stone-100 shadow-md">
+      <header className="flex items-center justify-between px-3 md:px-6 py-1 md:py-2 bg-stone-100 shadow-md">
         <NavLink to="/" className="text-xl font-semibold text-stone-800 ms-2">
           AWE
         </NavLink>
 
-        <nav className="flex gap-6 !text-gray-300 font-semibold text-sm">
+        <nav className="hidden md:flex gap-6 !text-gray-300 font-semibold text-sm">
           <NavLink
             to="/"
             className={({ isActive }) => {
@@ -65,6 +74,65 @@ const TopBar = () => {
           >
             Documents
           </NavLink>
+        </nav>
+        <nav className="md:hidden gap-6 text-sm">
+          <Sheet>
+            <SheetTrigger>
+              <AiOutlineMenu size={20} className="text-stone-800" />
+            </SheetTrigger>
+            <SheetContent className="flex flex-col items-start font-semibold pl-5 pt-12">
+              <NavLink
+                to="/"
+                className={({ isActive }) => {
+                  return isActive
+                    ? "text-black"
+                    : "text-gray-700 hover:text-stone-950";
+                }}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/events"
+                className={({ isActive }) => {
+                  return isActive
+                    ? "text-black"
+                    : "text-gray-700 hover:text-stone-950";
+                }}
+              >
+                Events
+              </NavLink>
+              <NavLink
+                to="/sds"
+                className={({ isActive }) => {
+                  return isActive
+                    ? "text-black"
+                    : "text-gray-700 hover:text-stone-950";
+                }}
+              >
+                SDS
+              </NavLink>
+              <NavLink
+                to="/non-muslims-day"
+                className={({ isActive }) => {
+                  return isActive
+                    ? "text-black"
+                    : "text-gray-700 hover:text-stone-950";
+                }}
+              >
+                Non-Muslims' Day
+              </NavLink>
+              <NavLink
+                to="/documents"
+                className={({ isActive }) => {
+                  return isActive
+                    ? "text-black"
+                    : "text-gray-700 hover:text-stone-950";
+                }}
+              >
+                Documents
+              </NavLink>
+            </SheetContent>
+          </Sheet>
         </nav>
       </header>
       <Routes>
